d2e52bd400decd78ca4a5e987da471dd36e703c7
Samuel Kupoe <62429600+iamkupoe@users.noreply.github.com>
2020-08-02 14:55:08 +0800

    Modiefied

--- src/components/About Us.js
+++ src/components/About Us.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
     return(
-      <h1></h1>
+      <h1>About Us</h1>
     );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
     return(
-       <div className="back">
-        
+       <div>
+        <h1 style={{marginLeft:'45%', marginTop:'3em', fontSize:'3em'}}>Home</h1>
        </div>
        
     );
